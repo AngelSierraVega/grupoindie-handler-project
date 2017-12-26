f861807d2881b4f205f623ea82bc8a51532cc031
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-12-26 02:23:01 -0600

    Created traits ToDeprecate and ToUpgrade

---
+++ src/GIndie/UnitTest/ClassTest/ToDeprecate.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * UnitTest - ToDeprecate
+ *
+ * @copyright (C) 2017 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package UnitTest
+ *
+ * 
+ */
+
+namespace GIndie\UnitTest\ClassTest;
+
+/**
+ * Description of ToDeprecate
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @version UT.00.00 17-12-26 Trait created
+ */
+trait ToDeprecate
+{
+
+
+
+}
