719d6df8ad4bdd73fc908f574c01fc5bab36bfa8
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2017-12-26 00:40:42 -0600

    Changed licenseheader autoloader.php and php. updated Created phar

--- phar.php
+++ phar.php
@@ -7,6 +7,19 @@
  *
  * @package UnitTest
  *
- * @version UT.00.2017.12.25
+ * @version UT.00.00 2017-12-25 File created
  */
 
+/**
+ * Crea un archivo phar
+ * @edit UT.00.01
+ * - Copy from GICommon
+ */
+$srcRoot = __DIR__ . "/src/GIndie/UnitTest";
+$buildRoot = __DIR__ . "/dist";
+$phar = new Phar($buildRoot . '/UnitTest.phar', 0, 'UnitTest.phar');
+$Directory = new RecursiveDirectoryIterator($srcRoot, FilesystemIterator::SKIP_DOTS);
+$Iterator = new RecursiveIteratorIterator($Directory);
+$phar->buildFromIterator($Iterator, $srcRoot);
+$phar->setStub($phar->createDefaultStub('autoloader.php'));
+echo "Archivo phar (/dist/UnitTest.phar) creado con éxito";
--- src/GIndie/UnitTest/autoloader.php
+++ src/GIndie/UnitTest/autoloader.php
@@ -6,7 +6,25 @@
  * @copyright (C) 2017 Angel Sierra Vega. Grupo INDIE.
  *
  * @package UnitTest
- *
- * @version UT.00.2017.12.25
+ * @version UT.00.00 2017-12-25 File created
  */
 
+namespace GIndie\UnitTest;
+
+/**
+ * Autoloader function
+ * 
+ * @edit UT.00.012017-12-26 
+ * - Copy from GICommon
+ */
+\spl_autoload_register(function($className) {
+    switch (\substr($className, 0, (\strlen(__NAMESPACE__) * 1)))
+    {
+        case __NAMESPACE__:
+            $edited = \substr($className, \strlen(__NAMESPACE__) + \strrpos($className, __NAMESPACE__));
+            $edited = \str_replace("\\", \DIRECTORY_SEPARATOR, __DIR__ . $edited) . ".php";
+            if (\is_readable($edited)) {
+                require_once($edited);
+            }
+    }
+});
